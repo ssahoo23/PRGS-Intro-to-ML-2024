36c4e43a70fc3246fafa9a8bd51de59744640b2a
srikant <ssahoo@pardeerand.edu>
2024-10-22 16:01:39 -0700

    HW02

---
+++ homework/Test.py
@@ -0,0 +1,3 @@
+print("hello world")
+import pandas as pd
+NRI_Table_Counties = pd.read_csv('C:/Users/ssahoo/Desktop/ML/PRGS-Intro-to-ML-2024/homework/data/raw/NRI_Table_Counties.csv', dtype={'STCOFIPS': str})
